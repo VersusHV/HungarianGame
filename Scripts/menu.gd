9ddce515b6e05299de7eb2e1a4ee71cc71b08de7
chawrona <dawidchawrona@gmail.com>
2025-02-06 03:14:39 +0100

    Deleted menu prototype, created proper menu, set menu as a main scene of the project

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,7 +1,9 @@
 extends Control
 
-func _on_start_game_pressed() -> void:
+
+func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
-func _on_quit_pressed() -> void:
+
+func _on_exit_pressed() -> void:
 	get_tree().quit()
