cf9fbe162ef8793d5ff9286b66d0ebdbf16cbe1d
VersusHV <versushv@gmail.com>
2025-02-13 17:50:16 +0100

    added test level with new player character, changes in player script

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,31 @@
-extends RigidBody3D
+extends CharacterBody3D
+
+@export_group("Camera")
+@export_range(0.0, 1.0) var mouse_sensitivity := 0.25
+
+@onready var camera_pivot: Node3D = %CameraPivot
+
+var	 _camera_input_direction := Vector2.ZERO
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 
-func _process(delta: float) -> void:
-	var input := Vector3.ZERO
-	input.x = Input.get_axis("move_left", "move_right")
-	input.z = Input.get_axis("move_forward", "move_back")
-	apply_central_force(input * 1200.0 * delta)
+func _unhandled_input(event: InputEvent) -> void:
+	var is_camera_motion := (
+		event is InputEventMouseMotion and
+		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	)
+	if is_camera_motion :
+		_camera_input_direction = event.screen_relative * mouse_sensitivity
+
+func _physics_process(delta: float) -> void:
+	camera_pivot.rotation.x += _camera_input_direction.y * delta
+	camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, -PI / 6.0, PI / 3.0)
+	camera_pivot.rotation.y -= _camera_input_direction.x * delta
+	
+	_camera_input_direction = Vector2.ZERO
+	
